b4c230d48edabc19f1e269bf904669377bf553f5
tiger-githubb <aristidetiger12@gmail.com>
2022-12-14 22:24:17 +0000

    js code

--- main.js
+++ main.js
@@ -1,2 +1,27 @@
 let columns = Math.floor(document.body.clientWidth / 50),
     rows = Math.floor(document.body.clientHeight / 50);
+
+const createTile = index => {
+    const tile = document.createElement('div');
+    tile.classList.add('tile');
+    return tile;
+
+}
+
+const createTiles = quantity => {
+    Array.from(array(quantity)).map((tile, index) => {
+        wrapper.appendChild(createTile(index));
+    })
+
+}
+
+createTiles(columns * rows);
+const createGrild = () => {
+    wrapper.innerHTML = "";
+    columns = Math.floor(document.body.clientWidth / 50);
+    rows = Math.floor(document.body.clientHeight / 50);
+
+    createTiles(columns * rows);
+}
+
+window.onresize = () => createGrild(); 
